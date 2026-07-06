12487384f221f65eb2aa84f256c1e41931a5bd66
ahmedbilla <ahmadbilla07@gmail.com>
2026-07-06 14:45:51 -0400

    Initial commit

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -2,4 +2,20 @@
 
 set -e
 
-service mariadb start
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+    mariadbd --user=mysql --bootstrap << EOF
+    CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+    CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+    GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+    FLUSH PRIVILEGES;
+EOF
+
+fi
+
+
+echo "Lancement de MariaDB..."
+exec mariadbd --user=mysql 
